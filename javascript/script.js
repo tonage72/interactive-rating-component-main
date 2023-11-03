7beb950c7e4652436369d35361d8d43699234552
David Fleming <tonage72@gmail.com>
2023-11-03 19:56:29 -0400

    Working on the JS

---
+++ javascript/script.js
@@ -0,0 +1,50 @@
+const circle1 = document.querySelector(".circle1");
+const circle2 = document.querySelector(".circle2");
+const circle3 = document.querySelector(".circle3");
+const circle4 = document.querySelector(".circle4");
+const circle5 = document.querySelector(".circle5");
+const submitButton = document.querySelector(".submit-button")
+
+function circles1(){
+	circle1.style.background = "hsl(25, 97%, 53%)";
+	circle2.style.background = "hsl(210, 2%, 24%)";
+	circle3.style.background = "hsl(210, 2%, 24%)";
+	circle4.style.background = "hsl(210, 2%, 24%)";
+	circle5.style.background = "hsl(210, 2%, 24%)";
+	submitButton.style.display = "block";
+}
+function circles2(){
+	circle1.style.background = "hsl(25, 97%, 53%)";
+	circle2.style.background = "hsl(25, 97%, 53%)";
+	circle3.style.background = "hsl(210, 2%, 24%)";
+	circle4.style.background = "hsl(210, 2%, 24%)";
+	circle5.style.background = "hsl(210, 2%, 24%)";
+	submitButton.style.display = "block";
+}
+function circles3(){
+	circle1.style.background = "hsl(25, 97%, 53%)";
+	circle2.style.background = "hsl(25, 97%, 53%)";
+	circle3.style.background = "hsl(25, 97%, 53%)";
+	circle4.style.background = "hsl(210, 2%, 24%)";
+	circle5.style.background = "hsl(210, 2%, 24%)";
+	submitButton.style.display = "block";
+}
+function circles4(){
+	circle1.style.background = "hsl(25, 97%, 53%)";
+	circle2.style.background = "hsl(25, 97%, 53%)";
+	circle3.style.background = "hsl(25, 97%, 53%)";
+	circle4.style.background = "hsl(25, 97%, 53%)";
+	circle5.style.background = "hsl(210, 2%, 24%)";
+	submitButton.style.display = "block";
+}
+function circles5(){
+	circle1.style.background = "hsl(25, 97%, 53%)";
+	circle2.style.background = "hsl(25, 97%, 53%)";
+	circle3.style.background = "hsl(25, 97%, 53%)";
+	circle4.style.background = "hsl(25, 97%, 53%)";
+	circle5.style.background = "hsl(25, 97%, 53%)";
+	submitButton.style.display = "block";
+}
+function submit(){
+	submitButton.style.background = "red";
+}
