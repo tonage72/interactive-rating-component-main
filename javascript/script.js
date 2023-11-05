2260dd5887152486fc1fb866c296bdd310297659
David Fleming <tonage72@gmail.com>
2023-11-05 00:57:26 -0400

    Redo of whole thing to make it simpler.

--- javascript/script.js
+++ javascript/script.js
@@ -1,58 +1,16 @@
-const circle1 = document.querySelector(".circle1");
-const circle2 = document.querySelector(".circle2");
-const circle3 = document.querySelector(".circle3");
-const circle4 = document.querySelector(".circle4");
-const circle5 = document.querySelector(".circle5");
 const submitButton = document.querySelector(".submit-button")
-const rating = document.querySelector(".rating")
-const thanks = document.querySelector(".thanks")
-const selection1 = document.querySelector(".selection1")
-const selection2 = document.querySelector(".selection2")
-const selection3 = document.querySelector(".selection3")
-const selection4 = document.querySelector(".selection4")
-const selection5 = document.querySelector(".selection5")
+const ratingCard = document.querySelector(".ratingCard")
+const thanksCard = document.querySelector(".thanksCard")
+const ratingCircle = document.querySelectorAll(".ratingCircle")
+const ratingResult = document.querySelector('span')
 
-function circles1(){
-	circle1.style.background = "hsl(25, 97%, 53%)";
-	circle2.style.background = "hsl(210, 2%, 24%)";
-	circle3.style.background = "hsl(210, 2%, 24%)";
-	circle4.style.background = "hsl(210, 2%, 24%)";
-	circle5.style.background = "hsl(210, 2%, 24%)";
-	submitButton.style.display = "block";
-}
-function circles2(){
-	circle1.style.background = "hsl(25, 97%, 53%)";
-	circle2.style.background = "hsl(25, 97%, 53%)";
-	circle3.style.background = "hsl(210, 2%, 24%)";
-	circle4.style.background = "hsl(210, 2%, 24%)";
-	circle5.style.background = "hsl(210, 2%, 24%)";
-	submitButton.style.display = "block";
-}
-function circles3(){
-	circle1.style.background = "hsl(25, 97%, 53%)";
-	circle2.style.background = "hsl(25, 97%, 53%)";
-	circle3.style.background = "hsl(25, 97%, 53%)";
-	circle4.style.background = "hsl(210, 2%, 24%)";
-	circle5.style.background = "hsl(210, 2%, 24%)";
-	submitButton.style.display = "block";
-}
-function circles4(){
-	circle1.style.background = "hsl(25, 97%, 53%)";
-	circle2.style.background = "hsl(25, 97%, 53%)";
-	circle3.style.background = "hsl(25, 97%, 53%)";
-	circle4.style.background = "hsl(25, 97%, 53%)";
-	circle5.style.background = "hsl(210, 2%, 24%)";
-	submitButton.style.display = "block";
-}
-function circles5(){
-	circle1.style.background = "hsl(25, 97%, 53%)";
-	circle2.style.background = "hsl(25, 97%, 53%)";
-	circle3.style.background = "hsl(25, 97%, 53%)";
-	circle4.style.background = "hsl(25, 97%, 53%)";
-	circle5.style.background = "hsl(25, 97%, 53%)";
-	submitButton.style.display = "block";
-}
-function submit(){
-	rating.style.display = "none";
-	thanks.style.display = "block";
-}
+submitButton.addEventListener('click', () => {
+	ratingCard.style.display = 'none';
+	thanksCard.style.display = 'block';
+});
+
+ratingCircle.forEach((ratingChosen) => {
+	ratingChosen.addEventListener('click', () => {
+	ratingResult.innerHTML = ratingChosen.innerHTML;
+	})
+});
